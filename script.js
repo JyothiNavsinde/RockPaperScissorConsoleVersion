8d300ccb561d942a3b9bcfe7708743ea79e9e407
Jyothi Navsinde <jyothinavsinde@gmail.com>
2025-04-24 14:15:04 +0530

    worte compChoice function to get a choice out of rock paper and scissor

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello world");
+// console.log("Hello world");
+function getComputerChoice() {
+    let compChoice = Math.floor(Math.random()*3);
+    console.log(compChoice);
+    if(compChoice == 0) {
+        return "rock";
+    } else if(compChoice == 1) {
+        return "paper";
+    } else {
+        return "scissor";
+    }
+}
